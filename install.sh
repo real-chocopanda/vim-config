74cc922bd2401106fbad6a2cb27d631c0eb8ae49
Christophe Moine <xophm1@gmail.com>
2011-09-29 13:26:25 +0200

    Remove now unnecessary install tasks.

--- install.sh
+++ install.sh
@@ -1,34 +1,14 @@
 #/bin/sh
 
-CURRENT=`pwd`
-
 # Git
 git submodule update --init
 git submodule foreach git pull origin master
 
-# Remove old installations
-if [ -d ~/.vim ] ; then
-    rm -f ~/.vim
-    rm -f ~/.vimrc
-    rm -f ~/.vim/doc/syntastic.txt
-    rm -f ~/.vim/plugin/syntastic.vim
-    rm -rf ~/.vim/syntax_checkers
-fi
-
-# Vim
-ln -s $CURRENT/.vim ~/.vim
-ln -s $CURRENT/.vimrc ~/.vimrc
-
-# Syntastic
-#cp -R $CURRENT/syntastic/doc/* ~/.vim/doc
-#cp -R $CURRENT/syntastic/plugin/* ~/.vim/plugin
-#cp -R $CURRENT/syntastic/syntax_checkers ~/.vim/syntax_checkers
-
 # Command-T
-cd ~/.vim/ruby/command-t
+cd ~/.vim/bundle-`uname -i`/command-t/ruby/command-t
 ruby extconf.rb
 make clean && make
-cd $CURRENT
+cd -
 
 # Here we go :)
 echo "Installed !"
